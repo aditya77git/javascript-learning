ecb96d25b9ffde95d71d45b2949391a94bac052e
aditya77git <adimali3773@gmail.com>
2024-10-23 21:22:48 +0530

    assigment 2
    variable-assignemnt2

---
+++ variable assignment 1.js
@@ -0,0 +1,19 @@
+// Variable Declaration and variable Intialization
+var cityName ="pune";
+console.log("City Name:",cityName);
+
+
+var bestfriendname="Amey";
+console.log("Best Friend Name:",bestfriendname);
+
+var countryname="India";
+console.log("Country Name:",countryname);
+
+var schoolname="Sayajirao School";
+console.log("School Name:",schoolname);
+
+var collegename="Y.C.I.S";
+console.log("College Name:",collegename);
+
+
+ 
---
+++ varible-assignment 2.js
@@ -0,0 +1,40 @@
+// Variable declaration and varibale Intialization
+
+var myFullName="Aditya Anand Mali";
+console.log("myFullName:",myFullName);
+
+var myFullName="Aditya Mali";
+console.log("myFullName:",myFullName);
+
+var FirstName="Aditya";
+console.log("First Name:",FirstName);
+
+var MiddleName="Anand";
+console.log("Middle Name:",MiddleName);
+
+var LastName="Mali";
+console.log("Last Name:",LastName);
+
+var language="I Love Only JS";
+console.log("Launguage:",language);
+
+var FirstName="Aditya";
+console.log("First Name:",FirstName);
+
+var LastName="Mali";
+console.log("Last Name:",LastName);
+
+var age="24";
+console.log("Age:",age);
+
+var address="Kothrud Pune";
+console.log("Address:",address);
+
+var pincode="411038";
+console.log("PinCode:",pincode);
+
+var gender="Male";
+console.log("Gender:",gender);
+
+var isMarried="false";
+console.log("ismarried:",isMarried);
